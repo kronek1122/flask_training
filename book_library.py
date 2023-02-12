472082b0c2024a598dec56812a99b4702744456f
Daniel <kronkowski.daniel@gmail.com>
2023-02-12 20:08:28 +0100

    new project

---
+++ book_library.py
@@ -0,0 +1,4 @@
+from book_lib import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
--- hello.py
+++ hello.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/') #dekorator wprowadzający funkcje na daną stronę
 def hello_world():
     return render_template('hello.html')
+
+def items():
+    response_data ={
+        'success':True,
+        'data': POSTS
+    }
+
+    return jsonify(response_data)
 
 @app.route('/another')
 def another():
